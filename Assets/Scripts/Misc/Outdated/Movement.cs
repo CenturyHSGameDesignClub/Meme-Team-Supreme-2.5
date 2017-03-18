38b31ca7829dacd92a754bd11ba9305cb7341ba2
ManaWanderer <manawanderer@gmail.com>
2017-03-18 14:16:06 -0700

    Reorganizing the files

---
+++ Assets/Scripts/Misc/Outdated/Movement.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/*
+ * 
+ * WARNING! Outdated, might not longed be in use
+ * 
+ * */
+
+public class Movement : MonoBehaviour {
+
+	public float InputDirection { get; set; }
+	public float JumpDirection { get; set;}
+	public bool IsReverse { get; set; }
+
+	public void SetGravitySettings(GravityState state)
+	{
+		if (GravityMagic.State == GravityMagic.GravityState.UP)					transform.rotation = Quaternion.Euler(new Vector3(-180, 0, 0));
+		else if (GravityMagic.State == GravityMagic.GravityState.DOWN)			transform.rotation = Quaternion.Euler(new Vector3(0, 0, 0));
+		else if (GravityMagic.State == GravityMagic.GravityState.LEFT)			transform.rotation = Quaternion.Euler(new Vector3(-90, 0, 0));
+		else if (GravityMagic.State == GravityMagic.GravityState.RIGHT)			transform.rotation = Quaternion.Euler(new Vector3(90, 0, 0));
+	}
+}
---
+++ Assets/Scripts/UI/Outdated/Pause.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+/*
+ * 
+ * WARNING! Outdated, might no longer be in use
+ * 
+ * */
+
+public class Pause : MonoBehaviour
+{
+//	[SerializeField]
+//	private GameObject pauseScreen;
+//    bool paused = false;
+//
+//
+//    void Update()
+//    {
+//		if (Input.GetKeyDown(KeyCode.Escape))
+//        {
+//            gamePaused();
+//        }
+//    }
+//
+//    public void gamePaused()
+//    {
+//        if (Time.timeScale == 1)
+//        {
+//            Time.timeScale = 0;
+//            paused = true;
+//        }
+//
+//        else
+//        {
+//            Time.timeScale = 1;
+//            paused = false;
+//        }
+//    }
+}
