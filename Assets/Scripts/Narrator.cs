03d9457bc33c36b53924e4dea44b6e26b48379fb
Wilson_Matteus <wilsm111@hsd.k12.or.us>
2017-04-03 09:27:02 -0700

    Added a script to the narrator and adjusted the text to explain the game a little better. We still need a title to put into the intro tutorial

--- Assets/Scripts/Narrator.cs
+++ Assets/Scripts/Narrator.cs
@@ -12,7 +12,8 @@ public class Narrator : MonoBehaviour
 	// Use this for initialization
 	void Start ()
     {
-        Tutorial();
+
+		StartCoroutine(Tutorial());
 	}
 	
 	// Update is called once per frame
@@ -27,13 +28,21 @@ public class Narrator : MonoBehaviour
         yield return new WaitForSeconds(5);
         Narration.text = "Press A and D to move left and right";
         yield return new WaitForSeconds(5);
-        Narration.text = "Press '(JumpButton)' to jump";
+        Narration.text = "Press Space to jump";
         yield return new WaitForSeconds(5);
-        Narration.text = "(Info about how to climb ladders)";
+        Narration.text = "Hold up or down to climb latters";
         yield return new WaitForSeconds(5);
         Narration.text = "Walk up to an object and press 'E' to pick up or repair that object.";
         yield return new WaitForSeconds(5);
-        Narration.text = "Have fun playing (Title), and do your best not to die, okay? However, I promise that if you die we will revive you as a zombie so that you can be studied to deduce why a trained engineer such as yourself was unable to repair a simple ship.";
+		Narration.text = "Get oxygen canisters to give more oxygen, but it maxes at 100.";
+		yield return new WaitForSeconds(5);
+		Narration.text = "Running out of oxygen or falling off the ship will kill you.";
+		yield return new WaitForSeconds(5);
+		Narration.text = "Use the scrap metal and torch to repair the holes in the ship.";
+		yield return new WaitForSeconds(5);
+		Narration.text = "Then you will be able to pick up the book, batteries and wrench, to restore power.";
+		yield return new WaitForSeconds(5);
+        Narration.text = "Have fun playing (Title), and do your best not to die, okay?";
         yield return new WaitForSeconds(10);
 
 
