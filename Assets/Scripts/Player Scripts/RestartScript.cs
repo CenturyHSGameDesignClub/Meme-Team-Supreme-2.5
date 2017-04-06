9968eff1b2469a900b3e8986297da31ae9b525dc
Wilson_Matteus <wilsm111@hsd.k12.or.us>
2017-04-06 16:16:44 -0700

    Made the restart it's own script so you can restart after you die

---
+++ Assets/Scripts/Player Scripts/RestartScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RestartScript : MonoBehaviour {
+
+	[SerializeField]
+	private string restartButton;
+
+
+	public bool Restart { get; set; }
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		Restart = Input.GetButton (restartButton);
+		if (Restart) {
+			//Restart the level
+			Application.LoadLevel(Application.loadedLevel);
+		}
+	}
+}
